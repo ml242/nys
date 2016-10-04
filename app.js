8b03d7f9edf9378ca8f8b342913685d6ae613502
matt lucas <mattlucas@gmail.com>
2016-10-04 15:09:26 -0400

    mysql is connected

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var app = express();
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  		host: 'localhost',
+			user: 'root',
+			port: 3306,
+			password: '',
+			database: 'nys_history',
+			multipleStatements: true
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
