411296fdff0f8f031b6b4115df7c583b07585d06
matt lucas <mattlucas@gmail.com>
2016-10-04 13:12:01 -0400

    node is working

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('NYS history coming to you live from the console!');
+});
