897e8a78f07c7597ed6c074e6b8e7d56b3ad19be
matt lucas <mattlucas@gmail.com>
2016-10-07 18:50:49 -0400

    back to vanilla start

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
-var app = express();
+var browserify = require('browserify');
+var React = require('react');
+var reactdom = require('react-dom')
+var jsx = require('node-jsx');
 var mysql      = require('mysql');
+
+var app = express();
+
+jsx.install();
+
 
 var connection = mysql.createConnection({
   		host: 'localhost',
@@ -13,7 +21,6 @@ var connection = mysql.createConnection({
 
 // Plot some points with a map  https://www.youtube.com/watch?v=7mkOVjRz3tg
 
-
 connection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
@@ -21,33 +28,31 @@ connection.connect(function(err) {
   }
 
   console.log('connected as id ' + connection.threadId);
-
-
   var query = 'SELECT * FROM places';
   connection.query(query, function(err, results, fields) {
-    if (err)
+    if (err) {
       return callback(err, null);
-
+    }
     console.log('The query-result is: ', results[0]);
-
-    // wrap result-set as json
     json = JSON.stringify(results);
-
   })
-
-
-
-
-
-
-
-
 });
 
-app.get('/', function (req, res) {
-  res.send(json);
+// app.get('/', function (req, res) {
+//   res.send(json);
+// });
+
+var Index = require('./views/index.jsx');
+
+
+app.use('/', function(req, res) {
+  res.send("hello")
 });
+
+
 
 app.listen(3000, function () {
-  console.log('NYS history coming to you live from the console!');
+  // var addr = server.address();
+  // console.log('NYS history coming to you live from @ http://%s:%d', addr.address, addr.port);
+  console.log('NYS history coming to you live');
 });
---
+++ views/index.jsx
@@ -0,0 +1,20 @@
+
+var React = require('react');
+
+var Index = React.createClass({
+  propTypes: {
+  },
+  getInitialState: function() {
+
+  },
+  handleChange: function(ev) {
+  },
+  render: function() {
+    return (
+      <html>
+      </html>
+    );
+  }
+});
+
+module.exports = Index;
